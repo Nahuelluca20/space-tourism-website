ed2cf59e210d1922551de5497bb7c5b8c0e79fe3
Nahuelluca20 <nahuelluca@gmail.com>
2021-12-19 17:03:34 -0300

    navbar and destination mobile ready

---
+++ scripts/destination-selector.js
@@ -0,0 +1,57 @@
+class Selector extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="selector">
+    <link rel="stylesheet" href="./styles/destination-selector.css">
+          <ul class="destination-list">
+            <li class="destination-item" aria-active="true" id="moon">
+              <a href="destination-moon.html">MOON</a>
+            </li>
+            <li class="destination-item" aria-active="false" id="mars">
+              <a href="destination-mars.html">MARS</a>
+            </li>
+            <li class="destination-item" aria-active="false" id="europa">
+              <a href="destination-europa.html">EUROPA</a>
+            </li>
+            <li class="destination-item" aria-active="false" id="titan">
+              <a href="destination-titan.html">TITAN</a>
+            </li>
+          </ul>
+        </div>
+    `;
+    const moon = document.getElementById("moon");
+    const mars = document.getElementById("mars");
+    const europa = document.getElementById("europa");
+    const titan = document.getElementById("titan");
+
+    const title = document.getElementById("destination-title");
+    const titleValue = title.firstChild.nodeValue;
+    console.log(titleValue)
+    if (titleValue === "MOON") {
+      moon.setAttribute("aria-active", true);
+      mars.setAttribute("aria-active", false);
+      europa.setAttribute("aria-active", false);
+      titan.setAttribute("aria-active", false);
+    } else if (titleValue === "MARS") {
+      moon.setAttribute("aria-active", false);
+      mars.setAttribute("aria-active", true);
+      europa.setAttribute("aria-active", false);
+      titan.setAttribute("aria-active", false);
+    } else if (titleValue === "EUROPA") {
+      moon.setAttribute("aria-active", false);
+      mars.setAttribute("aria-active", false);
+      europa.setAttribute("aria-active", true);
+      titan.setAttribute("aria-active", false);
+    } else if (titleValue === "TITAN") {
+      moon.setAttribute("aria-active", false);
+      mars.setAttribute("aria-active", false);
+      europa.setAttribute("aria-active", false);
+      titan.setAttribute("aria-active", true);
+    }
+  }
+}
+
+window.customElements.define("destination-selector", Selector);
---
+++ scripts/navbar.js
@@ -0,0 +1,62 @@
+class Nav extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <header class="primary-header flex">
+    <div>
+      <img src="/assets/shared/logo.svg" class="logo-nav" />
+    </div>
+
+    <button
+      class="mobile-nav-toggle"
+      aria-controls="primary-navigation"
+      aria-expanded="false"
+    >
+      <span class="sr-only"></span>
+    </button>
+
+    <nav id="navbar">
+      <link rel="stylesheet" href="../styles/navbar.css" />
+      <ul class="ul-nav flex" id="primary-navigation" data-visible="false">
+        <li class="nav-li">
+          <a href="/index.html" class="a-nav"> <span class="nav-span" aria-hidden="true">00</span>HOME</a>
+        </li>
+        <li class="nav-li">
+          <a href="/destination-moon.html" class="a-nav">
+            <span class="nav-span" aria-hidden="true">01</span>DESTINATION</a
+          >
+        </li>
+        <li class="nav-li">
+          <a href="" class="a-nav"> <span class="nav-span" aria-hidden="true">02</span>CREW </a>
+        </li>
+        <li class="nav-li">
+          <a href="" class="a-nav">
+            <span class="nav-span" aria-hidden="true">03</span>TECHNOLOGY</a
+          >
+        </li>
+      </ul>
+    </nav>
+    <!-- <img src="/assets/shared/icon-hamburger.svg" width="24px" height="21px" id="bt-nav" /> -->
+  </header>
+    `;
+
+    const primaryNav = document.querySelector(".ul-nav");
+    const navToggle = document.querySelector(".mobile-nav-toggle");
+
+    navToggle.addEventListener("click", () => {
+      const visibility = primaryNav.getAttribute("data-visible");
+
+      if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+      } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+      }
+    });
+  }
+}
+
+window.customElements.define("my-navigation", Nav);
